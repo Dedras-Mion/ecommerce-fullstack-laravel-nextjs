b9b01e90bd79128751abf18dba50809ed103864f
Dedras Mion <dedrasmion@gmail.com>
2024-03-22 12:01:00 +0900

    opensource

---
+++ frontend-nextjs/src/utils/fetch.js
@@ -0,0 +1,34 @@
+const getFetch = async (url, headers = {}) => {
+  const res = await fetch(`http://localhost:8000/api${url}`, {
+    cache: 'no-store',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+      ...headers,
+    },
+  });
+
+  if (res.ok) {
+    const data = await res.json();
+    return data.data;
+  } else {
+    throw new Error(`Problem on getting information:${res.status}`);
+  }
+};
+
+const postFetch = async (url, body, headers = {}) => {
+  const res = await fetch(`http://localhost:8000/api${url}`, {
+    method: 'POST',
+    cache: 'no-store',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+      ...headers,
+    },
+    body: JSON.stringify(body),
+  });
+
+  return await res.json();
+};
+
+export {getFetch, postFetch};
